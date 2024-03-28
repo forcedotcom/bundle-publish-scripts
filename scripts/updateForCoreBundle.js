bd4c9f8fcecc5aa196c274261fe3eeffc31f92d4
mingxuanzhang <mingxuanzhang@salesforce.com>
2024-03-28 14:17:07 -0700

    chore: refactor

--- scripts/updateForCoreBundle.js
+++ scripts/updateForCoreBundle.js
@@ -1,53 +1,40 @@
 const fs = require('fs');
 const path = require('path');
 
-const args = process.argv.slice(2);
-
-const projectName = args[0];
 // Function to update package.json
-function updatePackageJson(projectName) {
-  const packagePath = './package.json';
 
-  fs.readFile(packagePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(`Error reading package.json: ${err}`);
-      return;
+const packagePath = './package.json';
+const packageJson = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
+const packageName = packageJson.name;
+function updatePackageJson(packageJson) {
+  try {
+    // Update package name
+    if (packageJson.name) {
+      packageJson.name += '-bundle';
     }
 
-    try {
-      const packageJson = JSON.parse(data);
-
-      // Update package name if necessary
-      if(projectName === 'apex-node') {
-        if (packageJson.name && packageJson.name === '@salesforce/apex-node') {
-          packageJson.name = '@salesforce/apex-node-bundle';
-        } else {
-          console.error('Wrong package name or package.json does not exist.');
-        }
-      }
-
-      // Remove 'prepare' scripts
-      if (packageJson.scripts) {
-        delete packageJson.scripts.prepare;
-      }
-
-      // Update @salesforce/core dependency
-      if (packageJson?.dependencies["@salesforce/core"]) {
-        packageJson.dependencies["@salesforce/core-bundle"] = packageJson.dependencies["@salesforce/core"]
-        delete packageJson.dependencies["@salesforce/core"];
-      }
-
-      fs.writeFile(packagePath, JSON.stringify(packageJson, null, 2), 'utf8', (writeErr) => {
-        if (writeErr) {
-          console.error(`Error writing to package.json: ${writeErr}`);
-        } else {
-          console.log('package.json updated successfully.');
-        }
-      });
-    } catch (parseErr) {
-      console.error(`Error parsing JSON in package.json: ${parseErr}`);
+    // Remove 'pre*' scripts
+    if (packageJson.scripts) {
+      delete packageJson.scripts.prepare;
+      delete packageJson.scripts.prepack;
     }
-  });
+
+    // Update @salesforce/core dependency
+    if (packageJson?.dependencies["@salesforce/core"]) {
+      packageJson.dependencies["@salesforce/core-bundle"] = packageJson.dependencies["@salesforce/core"]
+      delete packageJson.dependencies["@salesforce/core"];
+    }
+
+    fs.writeFile(packagePath, JSON.stringify(packageJson, null, 2), 'utf8', (writeErr) => {
+      if (writeErr) {
+      console.error(`Error writing to package.json: ${writeErr}`);
+      } else {
+      console.log('package.json updated successfully.');
+      }
+    });
+  } catch (parseErr) {
+    console.error(`Error parsing JSON in package.json: ${parseErr}`);
+  }
 }
 
 function updateCoreImports() {
@@ -77,7 +64,34 @@ function updateCoreImports() {
   dirs.forEach((dir) => {
     traverseDirectory(dir);
   });
+  console.log('imports updated successfully');
 }
 
-updatePackageJson(projectName);
+function updateLoadMessages() {
+  const dirs = ['./src', './test'];
+  console.log('1111', packageName);
+  const updateUsagesWithinFile = (filePath) => {
+    let content = fs.readFileSync(filePath, 'utf8');
+    const regex = new RegExp(`Messages\\.loadMessages\\('${packageName}'`, 'g');
+    let modifiedContent = content.replace(regex, `Messages.loadMessages('${packageName}-bundle'`);
+    fs.writeFileSync(filePath, modifiedContent, 'utf8');
+  };
+  function traverseDirectory(directory) {
+    fs.readdirSync(directory).forEach((file) => {
+      const fullPath = path.join(directory, file);
+      if (fs.lstatSync(fullPath).isDirectory()) {
+        traverseDirectory(fullPath);
+      } else if (path.extname(fullPath) === '.ts') {
+        updateUsagesWithinFile(fullPath);
+      }
+    });
+  }
+  dirs.forEach((dir) => {
+    traverseDirectory(dir);
+  });
+  console.log('imports updated successfully');
+}
+
+updatePackageJson(packageJson);
 updateCoreImports();
+updateLoadMessages();
